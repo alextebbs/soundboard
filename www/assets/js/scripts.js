c64a30f75e5d2ad85a90ef635c1278b58aacd27e
Alex Tebbs <alex@alextebbs.com>
2019-07-12 17:03:37 -0700

    Finalize stuff

--- www/assets/js/scripts.js
+++ www/assets/js/scripts.js
@@ -59,6 +59,25 @@ loadJSON(function(response) {
 
   listener.register_many(keyCombos);
 
+  function formatAMPM(date) {
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0'+minutes : minutes;
+    var strTime = hours + ':' + minutes + ampm;
+    return strTime;
+  }
+
+  function startTime() {
+    var today = new Date();
+    document.querySelector('.time').innerHTML = formatAMPM(today);
+    var t = setTimeout(startTime, 500);
+  }
+
+  startTime();
+
 });
 
 
